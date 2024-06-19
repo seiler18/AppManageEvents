5f20264f6af3f901f11417b74d3735c701f13ba4
seiler18 <ichbinseiler@gmail.com>
2024-06-18 20:03:47 -0400

    Subiendo la aplicación a github

---
+++ src/main/java/cl/talentodigital/appmanageevents/AppManageEventsApplication.java
@@ -0,0 +1,13 @@
+package cl.talentodigital.appmanageevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppManageEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppManageEventsApplication.class, args);
+	}
+
+}
