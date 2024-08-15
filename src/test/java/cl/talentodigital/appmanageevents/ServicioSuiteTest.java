6cf2d06ba13e2e66d6a2001259ab5136c3f0bd20
seiler18 <ichbinseiler@gmail.com>
2024-08-14 22:36:46 -0400

    Añadiendo test a suiteTest

--- src/test/java/cl/talentodigital/appmanageevents/ServicioSuiteTest.java
+++ src/test/java/cl/talentodigital/appmanageevents/ServicioSuiteTest.java
@@ -1,5 +1,8 @@
 package cl.talentodigital.appmanageevents;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
@@ -9,4 +12,10 @@ public class ServicioSuiteTest {
     // Test suite for SalonServicioTest and EventoServicioTest
     // Run this class to run both test classes
     // This class should not contain any logic
+
+    @Test
+    public void testSuiteRuns() {
+        // This test just ensures that the suite runs
+        assertTrue(true, "The test suite is running.");
+    }
 }
